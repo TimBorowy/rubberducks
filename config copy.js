1997c44d6e5ef4d296890e312d02e463c6838521
Tim Borowy <blastertimbo@gmail.com>
2023-02-26 17:23:07 +0100

    add config file

---
+++ config copy.js
@@ -0,0 +1,13 @@
+const config = {
+  app: {
+    port: 1337
+  },
+  mqtt: {
+    host: 'hostname',
+    port: 1883,
+    username: 'user',
+    password: 'pass'
+  }
+ };
+ 
+ module.exports = config;
